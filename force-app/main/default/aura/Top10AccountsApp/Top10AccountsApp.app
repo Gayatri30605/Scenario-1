82a4991f296f603baa4fd46b5d56310ca20ed4d4
Gayatri30605 <gayatri.anil.dhumal@accenture.com>
2022-08-02 13:11:25 +0530

    Commit on

---
+++ force-app/main/default/aura/Top10AccountsApp/Top10AccountsApp.app
@@ -0,0 +1,3 @@
+<aura:application>
+    <c:Top10Accounts/>
+</aura:application>
--- force-app/main/default/lwc/top10Accounts/top10Accounts.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class Top10Accounts extends LightningElement {}
